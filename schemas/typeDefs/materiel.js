9df3bcf82ae76816913e50a9db3937e7660bc1f1
Ks Krimi <ny.kalash@gmail.com>
2022-04-24 10:29:05 +0200

    🌱  (schema) mutation in detail and materiel schema finished

--- schemas/typeDefs/materiel.js
+++ schemas/typeDefs/materiel.js
@@ -21,10 +21,46 @@ export default gql`
     status: AllowedStatus
   }
 
+  input AddDetailInput {
+    type: String!
+    marque: String!
+  }
+
+  input UpdateDetailInput {
+    type: String
+    marque: String
+  }
+
+  input AddMaterielInput {
+    serie: String!
+    detailId: String!
+    userId: String
+    technicienId: String
+    status: String
+  }
+
+  input UpdateMaterielInput {
+    serie: String
+    detailId: ID
+    userId: ID
+    technicienId: String
+    status: String
+  }
+
   extend type Query {
     materiels: [Materiel!]
     materiel(id: ID!): Materiel
     details: [Detail!]
     detail(id: ID!): Detail
+  }
+
+  extend type Mutation {
+    addDetail(addDetailFields: AddDetailInput!): Detail
+    updateDetail(id: ID!, updateMaterielFields: UpdateDetailInput!): Detail
+    deleteDetail(id: ID!): ID!
+
+    addMateriel(addMaterielFields: AddMaterielInput!): Materiel
+    updateMateriel(id: ID!, updateMaterielFields: UpdateMaterielInput): Materiel
+    deleteMateriel(id: ID!): ID!
   }
 `
